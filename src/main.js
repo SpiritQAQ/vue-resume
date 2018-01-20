282762680ca3837fb956e3a5bc8f94a01da81528
SpiritQAQ <444042073@qq.com>
2018-01-20 14:15:21 +0800

    leancloud-storage

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Vuex from 'vuex'
+import AV from 'leancloud-storage'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(Vuex)
-
+const appId = 'C5AXdInI1BOzXz0VCfBrsVfD-gzGzoHsz';
+const appKey = 'NgYXXvT9d48vLOgDpr6CLPVc';
+AV.init({ appId, appKey });
 
 /* eslint-disable no-new */
 new Vue({
